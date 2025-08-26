d5f2cdca2e634de2ddea59f07ef4759084e97452
kkt9102 <kkt9102@gmail.com>
2025-08-27 08:38:47 +0900

    feat : 가이드 테스트는 정상작동. 따로 개인서버로 테스트예정

--- app/testguide/page.tsx
+++ app/testguide/page.tsx
@@ -1,26 +1,26 @@
-type Repo = {
-  name: string;
-  stargazers_count: number;
-};
+// type Repo = {
+//   name: string;
+//   stargazers_count: number;
+// };
 
-async function getRepo() {
-  const res = await fetch("https://api.github.com/repos/vercel/next.js", {
-    cache: "no-store",
-  });
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  const repo: Repo = await res.json();
-  return repo;
-}
+// async function getRepo() {
+//   const res = await fetch("https://api.github.com/repos/vercel/next.js", {
+//     cache: "no-store",
+//   });
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+//   const repo: Repo = await res.json();
+//   return repo;
+// }
 
 export default async function Page() {
-  const repo = await getRepo();
+  // const repo = await getRepo();
 
   return (
     <div>
       <div>Next.js Repo Stars</div>
-      <div>{repo.stargazers_count}</div>
+      {/* <div>{repo.stargazers_count}</div> */}
       <div>
         (This page is server-side rendered at {new Date().toISOString()})
       </div>
